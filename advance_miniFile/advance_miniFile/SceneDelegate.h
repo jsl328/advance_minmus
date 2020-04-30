3e00c1d89fae1956c12a7401af0e0ff95d7f019a
jiangsl <jiangsl@yusys.com.cn>
2020-04-30 15:54:15 +0800

    git init commit

---
+++ advance_miniFile/advance_miniFile/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  advance_miniFile
+//
+//  Created by jsl on 2020/4/30.
+//  Copyright © 2020 jsl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ advance_miniFile/advance_miniFile/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  advance_miniFile
+//
+//  Created by jsl on 2020/4/30.
+//  Copyright © 2020 jsl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
